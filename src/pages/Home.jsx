ccb8eff4e3ebea5f0bc6fe0b74b10659491794fc
Md. Siam Shekh <siam62349@gmail.com>
2024-08-18 08:37:06 +0600

    home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,12 +19,13 @@ const Home = () => {
                     <div className="bg-gradient-to-l from-transparent to-[#1A103D] w-full h-full absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"></div>
                     <div className="bg-gradient-to-t from-transparent to-[#1A103D] w-full h-full absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"></div>
                 </div>
-                <div className="relative z-10 h-full w-full flex justify-center items-center">
+                <div className="relative z-10 h-full w-full flex justify-center items-center ">
                     <div className="">
-                        <p className="font-tektur md:text-5xl text-3xl font-bold mb-5">Search by Contract <span className="block bg-gradient-to-r from-blue-400 to-blue-700 bg-clip-text text-transparent">Address & Symbol</span></p>
+                        <p className="px-5 font-tektur md:text-5xl text-2xl  md:px-0 font-bold mb-5">Search by Contract </p>
                         <div className="border border-b-4 border-r-4 px-7 py-3 w-full flex justify-between" >
-                            <input type="text" placeholder="search by contract address" className="bg-transparent font-tektur outline-none " />
-                            <div className="btn rounded-full">
+                            <input type="text" placeholder="search" className="bg-transparent lg:w-fit w-[60vw] font-tektur outline-none " />
+
+                            <div className="md:btn md:rounded-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                 </svg>
@@ -34,9 +35,9 @@ const Home = () => {
                 </div>
             </section>
 
-            <section>
+            <section className="mx-5">
                 <p className="font-tektur text-2xl font-bold text-white">Markets</p>
-                <div className="grid grid-cols-3 gap-5 mt-2">
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mt-2">
                     {
                         isFetching ? <>
                             <div className="skeleton w-full h-52"></div>
@@ -55,7 +56,7 @@ const Home = () => {
                 </div>
             </section>
 
-            <section className="mt-5">
+            <section className="mt-5 mx-5">
                 {
                     isFetching ? <>
                         <div className="skeleton w-full h-[80vh]"></div>
